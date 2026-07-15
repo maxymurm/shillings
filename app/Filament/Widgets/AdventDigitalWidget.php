feed1ee39261c40cfa00daad5f9c90004c4f18a1
Maxwell Murunga <maxmm@adventdigital.co.ke>
2026-07-15 14:19:05 +0300

    feat(dashboard): replace Filament info card with Advent Digital branding widget

---
+++ app/Filament/Widgets/AdventDigitalWidget.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class AdventDigitalWidget extends Widget
+{
+    protected static string $view = 'filament.widgets.advent-digital-widget';
+
+    protected static ?int $sort = -2;
+
+    protected int|string|array $columnSpan = 1;
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -11,8 +11,8 @@ use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
+use App\Filament\Widgets\AdventDigitalWidget;
 use Filament\Widgets\AccountWidget;
-use Filament\Widgets\FilamentInfoWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -54,7 +54,7 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
                 AccountWidget::class,
-                FilamentInfoWidget::class,
+                AdventDigitalWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
